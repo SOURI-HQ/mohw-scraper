52bf46d1a780d26d12bb1d57ecec579d305fed69
SOURI-HQ <seconddan@wp.pl>
2022-01-27 20:00:50 +0100

    Initial commit

---
+++ src/main/java/com/souri/mohwscraper/MohwScraperApplication.java
@@ -0,0 +1,13 @@
+package com.souri.mohwscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohwScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohwScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souri/mohwscraper/MohwScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souri.mohwscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohwScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
