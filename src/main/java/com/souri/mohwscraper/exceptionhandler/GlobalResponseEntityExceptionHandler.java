355fdbc7805e076a922c8acee303609090f046c6
SOURI-HQ <seconddan@wp.pl>
2022-04-15 23:35:16 +0200

    Code refactoring

--- src/main/java/com/souri/mohwscraper/exceptionhandler/GlobalResponseEntityExceptionHandler.java
+++ src/main/java/com/souri/mohwscraper/exceptionhandler/GlobalResponseEntityExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.souri.mohwscraper.exceptionhandler;
 
 import com.souri.mohwscraper.exceptionhandler.exceptions.IncorrectURLException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalResponseEntityExceptionHandler {
@@ -23,6 +23,11 @@ public class GlobalResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {NullPointerException.class})
     public ResponseEntity<Object> handleNullPointerException(NullPointerException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(value = {TimeoutException.class})
+    public ResponseEntity<Object> handleTimeoutException(TimeoutException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
--- src/main/java/com/souri/mohwscraper/util/PlayerScraper.java
+++ src/main/java/com/souri/mohwscraper/util/PlayerScraper.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -34,7 +34,7 @@ public class PlayerScraper {
         return null;
     }
 
-    private String getPage(String url) throws IncorrectURLException {
+    private String getPage(String url) throws IncorrectURLException, TimeoutException {
         String chromedriverEnvVar = System.getenv("Chromedriver");
         System.setProperty("webdriver.chrome.driver", chromedriverEnvVar);
         ChromeOptions options = new ChromeOptions();
@@ -70,23 +70,22 @@ public class PlayerScraper {
         String playerID = fetchPlayerID(playerName);
         Map<String, String> playerOverview = new HashMap<>();
 
-//        try {
-            String url = baseUrl + "/soldier/" + playerName + "/stats/" + playerID + "/pc";
-            Document doc = Jsoup.parse(getPage(url));
-
-            //TODO: Come up with a solution to solving NullPointerException exceptions
-            List<Element> playerOverviewStats = doc.getElementById("overview-numbers").getElementsByTag("li");
+        String url = baseUrl + "/soldier/" + playerName + "/stats/" + playerID + "/pc";
+        Document doc = Jsoup.parse(getPage(url));
+        try {
+            List<Element> playerOverviewStats = doc.getElementById("overview-numbers").getElementsByTag("lis");
             String rank = doc.getElementById("overview-rank-number").text();
-            if (playerOverviewStats.isEmpty() || rank.isEmpty()) {
-                throw new NoSuchElementException("Overview stats not found");
+            if (playerOverviewStats.isEmpty()) {
+                throw new NoSuchElementException("");
             }
 
             playerOverviewStats.forEach(n -> playerOverview.put(n.getElementsByTag("h4").text(), n.getElementsByTag("p").text()));
             playerOverview.put("Rank", rank);
+        }
+        catch(NoSuchElementException e) {
+            throw new NoSuchElementException("Element not found");
+        }
 
-//        } catch (NullPointerException e) {
-//            e.printStackTrace();
-//        }
         return playerOverview;
     }
 
@@ -96,12 +95,19 @@ public class PlayerScraper {
         Map<String, String> playerDetails = new HashMap<>();
         String url = baseUrl + "/soldier/" + playerName + "/detailed/" + playerID + "/pc";
         Document doc = Jsoup.parse(getPage(url));
-        List<Element> playerDetailsStats = doc.getElementById("mohw-stats-detailed-stats").getElementsByTag("li");
-        if (playerDetailsStats.isEmpty()) {
-            throw new NoSuchElementException("Detailed stats not found");
+        try {
+            List<Element> playerDetailsStats = doc.getElementById("mohw-stats-detailed-stats").getElementsByTag("li");
+            //Handling situations when tag element in mohw-stats-detailed-stats is not found
+            //TODO: create custom exception for NoSuchElementException
+            if (playerDetailsStats.isEmpty()) {
+                throw new NoSuchElementException("");
+            }
+            playerDetailsStats.forEach(n -> playerDetails.put(n.text().substring(0, n.text().length() - n.getElementsByTag("strong").text().length()), n.getElementsByTag("strong").text()));
         }
-        playerDetailsStats.forEach(n -> playerDetails.put(n.text().substring(0, n.text().length() - n.getElementsByTag("strong").text().length()), n.getElementsByTag("strong").text()));
-
+        //Handling situations when mohw-stats-detailed-stats is not found
+        catch(NoSuchElementException e) {
+            throw new NoSuchElementException("Element not found");
+        }
         return playerDetails;
     }
 }
